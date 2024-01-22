7e91c78f4233d1fb1831f12e04aac3d123d01341
John Lenin <ziziz123ziziz@gmail.com>
2024-01-22 14:06:25 +0500

    update

--- appdb.py
+++ appdb.py
@@ -3,7 +3,7 @@ from app import Rule, User, Player, Group, Game, Season
 from sqlalchemy import text
 from sqlalchemy import delete
 from UserLogin import UserLogin
-from datetime import datetime
+
 
 app.app_context().push()
 
@@ -32,14 +32,34 @@ def add_group(g_name: str):
 s = db.session.query(Season).all()
 if not s:
     start_season("Cool season!")
+    s = db.session.query(Season).all()
 
 g = db.session.query(Group).first()
 if not g:
     add_group("S")
+    g = db.session.query(Group).first()
 
-p = g.players
-print(p)
+u = db.session.query(User).all()
+if not u:
+    n = 5
+    for i in range(n):
+        text = f"AbaCaba{i}"
+        user = User(
+           name = text,
+           password = text,
+           email = f"{text}@utmn.ru",
+           is_valid = True
+        )
+        db.session.add(user)
+    db.session.commit()
+    u = db.session.query(User).all()
 
+p = g.players.all()
+if not p:
+    for i in u:
+        g.players.add(i)
+    db.session.commit()
+    p = g.players.all()
 
 
 
