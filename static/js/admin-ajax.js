363504df3d9f751d990db9fbb04673b2c342b8af
tengenatari <152083348+tengenatari@users.noreply.github.com>
2024-02-08 14:33:50 +0500

    Add files via upload

--- static/js/admin-ajax.js
+++ static/js/admin-ajax.js
@@ -1,5 +1,21 @@
 $(document).ready(function (){
    $(document.getElementById('select-menu')).on('change', function (){
+         let value = $(this).value;
+      req = $.ajax({url:'/update-admin-page', table:value});
+      req.done(function (data){
+         let element = document.getElementById('Table')
+         if (element){
+            element.parentNode.removeChild(element)
+            let div = document.createElement('Table');
+                div.className = 'Table'
+                div.id = 'Table'
 
+               div.innerHTML = data['page']
+               document.getElementById('cont').append(div)
+         }
+      });
+   });
+   $(document).on('change', '.box', function(){
+      
    });
 });
